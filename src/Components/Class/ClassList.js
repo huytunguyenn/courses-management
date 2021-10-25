bbcd1af68bb09c82b522f472a89fd79c8cc28fb6
huytunguyenn <49774218+huytunguyenn@users.noreply.github.com>
2021-10-26 00:52:12 +0700

    init

---
+++ src/Components/Class/ClassList.js
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+
+export default  () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/classes")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    return(
+        <ul>
+            {items.map(cls => <li>{cls.className}</li>)}
+        </ul>
+    );
+}
